d2bb004b80eea75712a2156418767b6954bc9998
pr4j3sh <prajesh.eleven118@gmail.com>
2025-05-06 12:57:04 +0530

    feat: sorted templates, added new temp

--- docs/templates/template.js
+++ docs/templates/template.js
@@ -1,79 +1,8 @@
-const items = [
-  {
-    repo: "next-tw",
-    title: "NextJs + Tailwind",
-    tech: ["NextJs", "Javascript", "Tailwind", "Frontend"],
-    url: "https://next-tw-frames.vercel.app/",
-  },
-  {
-    repo: "js-docs",
-    title: "Javascript Documentation",
-    tech: ["Docusaur", "JsDoc", "Documentation", "Javascript"],
-  },
-  {
-    repo: "python",
-    title: "Python Package/Library",
-    tech: ["Python", "Library", "Package", "PIP"],
-  },
-  {
-    repo: "cpp",
-    title: "CPP Package/Library",
-    tech: ["CPP", "Library", "Package", "Make"],
-  },
-  {
-    repo: "c",
-    title: "C Package/Library",
-    tech: ["C", "Library", "Package", "Make"],
-  },
-  {
-    repo: "javascript",
-    title: "Javascript NPM Package/Library",
-    tech: ["Javascript", "Library", "Package", "NPM"],
-  },
-  {
-    repo: "rust",
-    title: "Rust Package/Library",
-    tech: ["Rust", "Library", "Package", "Crate"],
-  },
-  {
-    repo: "express-vercel",
-    title: "Express + Vercel",
-    tech: ["Javascript", "API", "Express", "Vercel", "Backend", "Server"],
-    url: "https://express-vercel-frames.vercel.app/",
-  },
-  {
-    repo: "express",
-    title: "Express",
-    tech: ["Javascript", "API", "Express", "Backend", "Server"],
-  },
-  {
-    repo: "js-tw",
-    title: "Javascript + Tailwind",
-    tech: ["Javascript", "Tailwind", "HTML", "CSS", "Frontend", "Vanilla"],
-    url: "https://pr4j3sh.github.io/js-tw/",
-  },
-  {
-    repo: "react-tw",
-    title: "React + Tailwind",
-    tech: ["React", "Tailwind", "React Router", "Frontend"],
-    url: "https://react-tw-frames.vercel.app/",
-  },
-  {
-    repo: "react-firebase",
-    title: "React + Firebase",
-    tech: [
-      "React",
-      "Firebase",
-      "Tailwind",
-      "React Router",
-      "Redux Toolkit",
-      "Frontend",
-    ],
-    url: "https://react-firebase-frames.vercel.app/",
-  },
-];
+import data from "./data.js";
 
 const templates = document.getElementById("templates");
+
+const items = data.sort((a, b) => a.title.localeCompare(b.title));
 
 const html = items
   .map((item) => {
